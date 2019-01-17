9aa2f24c1577a2d34f21225747a20b865e122ca3
ToxicDon <36772522+ToxicDon@users.noreply.github.com>
2019-01-17 12:42:54 -0500

    Add files via upload

--- Game Board/sketch.js
+++ Game Board/sketch.js
@@ -11,7 +11,7 @@
 // server variables
 
 
-
+var birdcolour = ["blue","red","green","yellow"];
 
 var birdN = 4;
 var birdS = 3;
@@ -20,10 +20,6 @@ var birdsBlue = [];
 var birdsRed = [];
 var birdsYellow = [];
 var birdsGreen = [];
-
-
-
-
 
 var dataServer;
 var pubKey = 'pub-c-a705a585-4407-4f88-8b83-ac846c45e13a';
@@ -69,7 +65,6 @@ function setup() {
   function draw() {
 
 
-
   rectMode(CORNER);
   //red section
   fill('#FF3333');
@@ -106,6 +101,9 @@ function setup() {
 
   strokeWeight(0);
 
+
+background(0,0,0,50)
+
 ellipseMode(RADIUS);
 
 
@@ -113,12 +111,9 @@ var speed = 1.5;
 var r = 15;
 
 //characters
-  fill('#006600');
-
+  // fill('#006600');
+fill("blue");
 ellipse(x,y,r);
-
-// x = constrain(x);
-console.log(constrain(x,100,cs-100));
 
   if (keyIsDown(LEFT_ARROW)) {
     x -= speed;
@@ -140,12 +135,31 @@ y = constrain(y,100+r,width-100-r);
 
 
 
-for(let i=0;i<4;i++){
-    // stroke(map(BirdieB[i].x,0,width,0,255),0,0);
-
+for(let i=0;i<birdsBlue.length;i++){
     fill("blue");
     birdsBlue[i].move();
     birdsBlue[i].show();
+}
+
+for(let i=0;i<birdsRed.length;i++){
+     fill("red");
+    birdsRed[i].move();
+    birdsRed[i].show();
+}
+
+for(let i=0;i<birdsGreen.length;i++){
+    fill("green");
+    birdsGreen[i].move();
+    birdsGreen[i].show();
+}
+for(let i=0;i<birdsYellow.length;i++){
+    fill("yellow");
+    birdsYellow[i].move();
+    birdsYellow[i].show();
+}
+
+/*
+
     fill("red");
     birdsRed[i].move();
     birdsRed[i].show();
@@ -156,6 +170,15 @@ for(let i=0;i<4;i++){
     birdsYellow[i].move();
     birdsYellow[i].show();
 
+}
+*/
+
+birdCatchB();
+birdCatchR();
+birdCatchY();
+birdCatchG();
+
+
 
 }
 
@@ -165,9 +188,101 @@ for(let i=0;i<4;i++){
 
 
 
+
+
+
+
+
+
+
+function birdCatchB(){
+  for(let i=0;i<birdsBlue.length;i++){
+    if(dist(birdsBlue[i].x,birdsBlue[i].y,x,y) <= bsize+15){
+      birdsBlue.splice(i,1);
+
+    }
+  }
 }
 
 
+
+
+
+function birdCatchR(){
+  for(let i=0;i<birdsRed.length;i++){
+    if(dist(birdsRed[i].x,birdsRed[i].y,x,y) <= bsize+15){
+      birdsRed.splice(i,1);
+    }
+  }
+
+}
+function birdCatchY(){
+  for(let i=0;i<birdsYellow.length;i++){
+    if(dist(birdsYellow[i].x,birdsYellow[i].y,x,y) <= bsize+15){
+      birdsYellow.splice(i,1);
+    }
+  }
+}
+function birdCatchG(){
+  for(let i=0;i<birdsGreen.length;i++){
+    if(dist(birdsGreen[i].x,birdsGreen[i].y,x,y) <= bsize+15){
+      birdsGreen.splice(i,1);
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//-------Bird Creation
+
+var bsize = 10;
+var bspeed = 3;
 
 class BirdieB{
   constructor(x,y) {
@@ -175,41 +290,38 @@ class BirdieB{
 
     this.x = x;
     this.y = y;
-    this.xs = 3*num;
-        num *= Math.floor(Math.random()*2) == 1 ? 1 : -1;
+    this.xs = bspeed*num;
+    num *= Math.floor(Math.random()*2) == 1 ? 1 : -1;
 
-    this.ys = 3*num;
+    this.ys = bspeed*num;
   }
   move(){
     this.x += this.xs;
     this.y += this.ys;
-    if(this.x >= cs){
-      this.x = cs;
+
+    if(this.x >= cs-bsize){
+      this.x = cs-bsize;
       this.xs*= -1;
     }
-    if(this.y >= cs){
-      this.y = cs;
+    if(this.y >= cs-bsize){
+      this.y = cs-bsize;
       this.ys*= -1;
     }
 
-    if(this.x<=0){
-      this.x = 0;
+    if(this.x<=0+bsize){
+      this.x = 0+bsize;
       this.xs *= -1; 
     }
     if(this.y<=0){
-      this.y = 0;
+      this.y = 0+bsize;
       this.ys *= -1;
     }
 
-
   }
   show(){
-    ellipse(this.x,this.y, 10,10);
+    ellipse(this.x,this.y, bsize);
   }
 }
-
-
-
 
 class BirdieR{
 
@@ -218,48 +330,41 @@ class BirdieR{
 
     this.x = x;
     this.y = y;
-    this.xs = 3*num;
+    this.xs = bspeed*num;
         num *= Math.floor(Math.random()*2) == 1 ? 1 : -1;
 
-    this.ys = 3*num;
+    this.ys = bspeed*num;
   }
 
   move(){
     this.x += this.xs;
     this.y += this.ys;
-    if(this.x >= cs){
-      this.x = cs;
+
+    if(this.x >= cs-bsize){
+      this.x = cs-bsize;
       this.xs*= -1;
     }
-    if(this.y >= cs){
-      this.y = cs;
+    if(this.y >= cs-bsize){
+      this.y = cs-bsize;
       this.ys*= -1;
     }
 
-    if(this.x<=0){
-      this.x = 0;
+    if(this.x<=0+bsize){
+      this.x = 0+bsize;
       this.xs *= -1; 
     }
     if(this.y<=0){
-      this.y = 0;
+      this.y = 0+bsize;
       this.ys *= -1;
     }
-
-
 
 
   }
 
   show(){
-    ellipse(this.x,this.y, 10,10);
+    ellipse(this.x,this.y, bsize);
   }
 }
-
-
-
-
-
-
 
 
 class BirdieG{
@@ -269,43 +374,40 @@ class BirdieG{
 
     this.x = x;
     this.y = y;
-    this.xs = 3*num;
+    this.xs = bspeed*num;
         num *= Math.floor(Math.random()*2) == 1 ? 1 : -1;
 
-    this.ys = 3*num;
+    this.ys = bspeed*num;
   }
 
   move(){
     this.x += this.xs;
     this.y += this.ys;
-    if(this.x >= cs){
-      this.x = cs;
+
+    if(this.x >= cs-bsize){
+      this.x = cs-bsize;
       this.xs*= -1;
     }
-    if(this.y >= cs){
-      this.y = cs;
+    if(this.y >= cs-bsize){
+      this.y = cs-bsize;
       this.ys*= -1;
     }
 
-    if(this.x<=0){
-      this.x = 0;
+    if(this.x<=0+bsize){
+      this.x = 0+bsize;
       this.xs *= -1; 
     }
     if(this.y<=0){
-      this.y = 0;
+      this.y = 0+bsize;
       this.ys *= -1;
     }
 
 
   }
   show(){
-    ellipse(this.x,this.y, 10,10);
+    ellipse(this.x,this.y, bsize);
   }
 }
-
-
-
-
 
 
 class BirdieY{
@@ -315,38 +417,38 @@ class BirdieY{
 
     this.x = x;
     this.y = y;
-    this.xs = 3*num;
+    this.xs = bspeed*num;
         num *= Math.floor(Math.random()*2) == 1 ? 1 : -1;
 
-    this.ys = 3*num;
+    this.ys = bspeed*num;
 
   }
 
   move(){
     this.x += this.xs;
     this.y += this.ys;
-    if(this.x >= cs){
-      this.x = cs;
+
+    if(this.x >= cs-bsize){
+      this.x = cs-bsize;
       this.xs*= -1;
     }
-    if(this.y >= cs){
-      this.y = cs;
+    if(this.y >= cs-bsize){
+      this.y = cs-bsize;
       this.ys*= -1;
     }
 
-    if(this.x<=0){
-      this.x = 0;
+    if(this.x<=0+bsize){
+      this.x = 0+bsize;
       this.xs *= -1; 
     }
     if(this.y<=0){
-      this.y = 0;
+      this.y = 0+bsize;
       this.ys *= -1;
     }
 
-
   }
   show(){
-    ellipse(this.x,this.y, 10,10);
+    ellipse(this.x,this.y, bsize);
   }
 }
 
